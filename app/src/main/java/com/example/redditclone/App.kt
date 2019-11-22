a5e590fccc9d68669f68c5c46e3193953d4429b1
Danielius <dnlspasin@gmail.com>
2019-11-22 20:05:05 +0200

    remember user

--- app/src/main/java/com/example/redditclone/App.kt
+++ app/src/main/java/com/example/redditclone/App.kt
@@ -1,10 +1,18 @@
 package com.example.redditclone
 
+import android.content.SharedPreferences
 import com.example.redditclone.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import javax.inject.Inject
 
 class App : DaggerApplication() {
+   @Inject lateinit var preferences: SharedPreferences
+    var isLoggedIn = true
+    override fun onCreate() {
+        super.onCreate()
+       isLoggedIn = !preferences.getString("access_token", "").isNullOrEmpty()
+    }
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
--- app/src/main/java/com/example/redditclone/ui/MainActivity.kt
+++ app/src/main/java/com/example/redditclone/ui/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.redditclone.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
+import com.example.redditclone.App
 import com.example.redditclone.R
-import dagger.android.DaggerActivity
+import com.example.redditclone.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        navController = findNavController(R.id.main_fragment_container)
+        if ((application as App).isLoggedIn) navController.navigate(R.id.action_global_main_nav)
+
     }
 }
